e11d89222df4a6e6eaf47384c1e42bd5cceb8df1
Aymen Benyoub <aymen.Benyoub03@gmail.com>
2026-02-23 14:11:40 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,17 @@
+// core: single storage node internals
+
+// cluster: cluster management, node discovery, cluster membership and other distributed system related features
+
+// main.go: entry point for the application
+
+package main 
+
+import "fmt"
+
+
+
+
+func main(){
+
+	fmt.Println("Application started...")
+}
