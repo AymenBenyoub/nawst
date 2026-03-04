aee44039fbaf9aa089310103e5db6ba7709731af
Aymen Benyoub <aymen.Benyoub03@gmail.com>
2026-03-04 11:40:27 +0100

    add newline for rpc prints

--- core/server.go
+++ core/server.go
@@ -86,7 +86,7 @@ func (s *Server) sendRequest(ctx context.Context, req Request) Response {
 
 // gRPC Put RPC
 func (s *Server) Put(ctx context.Context, req *pb.PutRequest) (*emptypb.Empty, error) {
-	fmt.Printf("RPC Put %s length: %d val: %v", req.Key, len(req.Value), string(req.Value))
+	fmt.Printf("RPC Put %s length: %d val: %v\n", req.Key, len(req.Value), string(req.Value))
 	resp := s.sendRequest(ctx, Request{
 		Op:           OpPut,
 		Key:          req.Key,
@@ -113,13 +113,13 @@ func (s *Server) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, 
 		}
 		return nil, status.Errorf(codes.Internal, "Failed to get key: %v", resp.Err)
 	}
-	fmt.Printf("RPC Get %s length: %d val: %v", req.Key, len(resp.Value), string(resp.Value))
+	fmt.Printf("RPC Get %s length: %d val: %v\n", req.Key, len(resp.Value), string(resp.Value))
 	return &pb.GetResponse{Value: resp.Value}, nil
 }
 
 // gRPC Delete RPC
 func (s *Server) Delete(ctx context.Context, req *pb.DeleteRequest) (*emptypb.Empty, error) {
-	fmt.Printf("RPC Delete %s", req.Key)
+	fmt.Printf("RPC Delete %s\n", req.Key)
 	resp := s.sendRequest(ctx, Request{
 		Op:           OpDelete,
 		Key:          req.Key,
